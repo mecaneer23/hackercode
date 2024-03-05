3a63a5d0e614da06d83f6417e392b67fc0ea983e
mecaneer23 <mecaneer23@gmail.com>
2024-03-05 16:36:47 -0600

    refactor: autoformat

--- HackerCode.java
+++ HackerCode.java
@@ -1,1 +1,7 @@
-public class HackerCode {public static void main(String[] args) {while (true) {System.out.print(Math.round(Math.random()));}}}
+public class hackercode {
+	public static void main(String[] args) {
+		while (true) {
+			System.out.print(Math.round(Math.random()));
+		}
+	}
+}
